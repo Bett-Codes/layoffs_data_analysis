aa20d00549204039c915f9e98654d91b8da24733
Bett-Codes <kipkoechdenniskd@gmail.com>
2026-02-24 16:45:37 +0300

    initial commit

---
+++ exploratory_data_analysis.sql
@@ -0,0 +1,67 @@
+-- exploratory data analysis
+select max(total_laid_off), max(percentage_laid_off)
+from layoffs_staging2;
+
+ select *
+ from layoffs_staging2
+ where percentage_laid_off = 1
+ order by funds_raised_millions desc;
+ 
+ select company, sum(total_laid_off)
+ from layoffs_staging2
+ group by company
+ order by 2 desc;
+ 
+ select country, sum(total_laid_off)
+ from layoffs_staging2
+ group by country
+ order by 2 desc;
+ 
+ select year(`date`), sum(total_laid_off)
+ from layoffs_staging2
+ group by year(`date`)
+ order by 1 desc;
+ 
+ select stage, sum(total_laid_off)
+ from layoffs_staging2
+ group by stage
+ order by 2 desc ;
+ 
+ select substring(`date`,1,7) as `MONTH`, sum(total_laid_off)
+ from layoffs_staging2
+ where substring(`date`,1,7) is not null
+ group by `MONTH`
+ order by 1 asc;
+ 
+ with Rolling_total as
+ (
+ select substring(`date`,1,7) as `MONTH`, sum(total_laid_off) as total_off
+ from layoffs_staging2
+ where substring(`date`,1,7) is not null
+ group by `MONTH`
+ order by 1 asc
+ )
+ select `MONTH`, total_off, sum(total_off) over(order by `MONTH`) as rolling_total
+ from Rolling_total;
+ 
+with Company_Year(company, years, total_laid_off) as 
+(
+ select company, year(`date`), sum(total_laid_off)
+ from layoffs_staging2
+ group by company, year(`date`)
+ order by company asc
+ ), Company_Year_Rank as
+ (
+ select *, dense_rank() over(partition by years order by total_laid_off desc) as ranking
+ from Company_Year
+ where years is not null
+ )
+ select *
+ from Company_Year_Rank
+ where ranking <= 5
+ 
+ 
+ 
+ 
+ 
+ 
